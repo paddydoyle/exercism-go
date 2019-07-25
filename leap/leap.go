7b2367418a0e190050b4f3580e70a0b507da0eba
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-07-25 20:57:21 +0100

    leap: swap conditions; a bit faster

--- leap/leap.go
+++ leap/leap.go
@@ -7,5 +7,5 @@ package leap
 
 // IsLeapYear should have a comment documenting it.
 func IsLeapYear(year int) bool {
-	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+	return (year%100 != 0 || year%400 == 0) && year%4 == 0
 }
