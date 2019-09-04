a3860bdcf24f52bb76d95206ec8b437baf1d797e
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-09-04 23:22:44 +0100

    hamming: solution

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,20 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
+	distance := 0
+
+	if len(a) != len(b) {
+		return distance, errors.New("Inputs of unequal lengths.")
+	}
+
+	for pos, charA := range(a) {
+		// Must convert to avoid rune/byte mismatch
+		if byte(charA) != b[pos] {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
