56fb87a4e1e78f7f99af78d32aa85a7db992696e
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-09-06 23:00:53 +0100

    triangle: working

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -19,28 +19,22 @@ const (
 
 // KindFromSides should have a comment documenting it.
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	//var k Kind
-
 	// All sides zero length?
 	if (a + b + c) == 0 {
 		return NaT
 	}
 
 	// Use a list to find the largest value
-	sidesList := [3]float64{a, b, c}
-	sort.Float64s(sidesList[:])
+	sidesList := []float64{a, b, c}
+	sort.Float64s(sidesList)
 
 	// Any side length a NaN? It's always put first in a sorted list.
-	if sidesList[0] == math.NaN() {
+	if math.IsNaN(sidesList[0]) {
 		return NaT
 	}
 
 	// Any side length a Inf?
-	if sidesList[0] == math.Inf(-1) || sidesList[2] == math.Inf(1) {
+	if math.IsInf(sidesList[0], -1) || math.IsInf(sidesList[2], 1) {
 		return NaT
 	}
 
