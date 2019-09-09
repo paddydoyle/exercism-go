fe385169186f814143509c3b4c47d3aca1827ba6
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-09-09 21:20:42 +0100

    rna-transcription: solution

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,4 +1,20 @@
 package strand
 
+import "bytes"
+
 func ToRNA(dna string) string {
+	sidesMap := map[rune]rune{
+		'G': 'C',
+		'C': 'G',
+		'T': 'A',
+		'A': 'U',
+	}
+
+	buf := bytes.Buffer{}
+
+	for _, ch := range dna {
+		buf.WriteRune(sidesMap[ch])
+	}
+
+	return buf.String()
 }
