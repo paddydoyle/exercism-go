a67224de7955e68e1fe1615157840cf8636dac0e
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-09-14 17:04:53 +0100

    hamming: can drop the default var

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -7,7 +7,7 @@ func Distance(a, b string) (distance int, err error) {
 		return distance, errors.New("inputs of unequal lengths")
 	}
 
-	for i, _ := range a {
+	for i := range a {
 		if a[i] != b[i] {
 			distance++
 		}
