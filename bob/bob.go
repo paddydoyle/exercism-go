645832e5b4566cc592921a8940afde91d52f7b7b
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-09-16 18:32:57 +0100

    bob: working; think it's slow

--- bob/bob.go
+++ bob/bob.go
@@ -5,11 +5,41 @@
 // https://golang.org/doc/effective_go.html#commentary
 package bob
 
-// Hey should have a comment documenting it.
+import (
+	"strings"
+	"unicode"
+)
+
+// Hey returns a string according to the rules of Bob.
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	remark = strings.TrimSpace(remark)
+
+	switch {
+	case len(remark) == 0:
+		return "Fine. Be that way!"
+	case IsUpperString(remark) && strings.HasSuffix(remark, "?"):
+		return "Calm down, I know what I'm doing!"
+	case IsUpperString(remark):
+		return "Whoa, chill out!"
+	case strings.HasSuffix(remark, "?"):
+		return "Sure."
+	default:
+		return "Whatever."
+	}
+}
+
+// IsUpperString tests if the characters in the string are only uppercase
+func IsUpperString(remark string) bool {
+	containsAnyUppercase := false
+
+	for _, ch := range remark {
+		if unicode.IsLetter(ch) && unicode.IsLower(ch) {
+			return false
+		}
+		if !containsAnyUppercase && unicode.IsLetter(ch) {
+			containsAnyUppercase = true
+		}
+	}
+
+	return containsAnyUppercase
 }
