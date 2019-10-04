132f93598f98c530de4c81bec7f8f1e039990475
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-10-04 12:32:36 +0100

    bob: back to string/unicode version

--- bob/bob.go
+++ bob/bob.go
@@ -6,54 +6,12 @@
 package bob
 
 import (
-	"regexp"
 	"strings"
 	"unicode"
 )
 
 // Hey returns a string according to the rules of Bob.
-// 280000ns / op
-//=== RUN   TestHey
-//--- PASS: TestHey (0.00s)
-//goos: linux
-//goarch: amd64
-//BenchmarkHey-8              4274            280134 ns/op          247985 B/op       3475 allocs/op
-//PASS
-//ok      _/home/paddy/exercism/go/bob    2.148s
 func Hey(remark string) string {
-	//remark = strings.TrimSpace(remark)
-	isEmpty := regexp.MustCompile(`^\s*$`).MatchString
-	hasChars := regexp.MustCompile(`[[:alpha:]]`).MatchString
-	//hasUpperChars := regexp.MustCompile(`[A-Z]`).MatchString
-	hasLowerChars := regexp.MustCompile(`[a-z]`).MatchString
-	isUpperQuestion := regexp.MustCompile(`^[A-Z ]+\?\s*$`).MatchString
-	//isUpperString := regexp.MustCompile(`^[A-Z0-9 !,]+\s*$`).MatchString
-	isQuestion := regexp.MustCompile(`\?\s*$`).MatchString
-
-	switch {
-	case isEmpty(remark):
-		return "Fine. Be that way!"
-	case hasChars(remark) && isUpperQuestion(remark):
-		return "Calm down, I know what I'm doing!"
-	case hasChars(remark) && !hasLowerChars(remark):
-		return "Whoa, chill out!"
-	case isQuestion(remark):
-		return "Sure."
-	default:
-		return "Whatever."
-	}
-}
-
-// Hey returns a string according to the rules of Bob.
-// 900-1000ns / op
-//=== RUN   TestHey
-//--- PASS: TestHey (0.00s)
-//goos: linux
-//goarch: amd64
-//BenchmarkHey-8           1306461               912 ns/op               0 B/op          0 allocs/op
-//PASS
-//ok      _/home/paddy/exercism/go/bob    1.976s
-func Hey2(remark string) string {
 	remark = strings.TrimSpace(remark)
 
 	switch {
