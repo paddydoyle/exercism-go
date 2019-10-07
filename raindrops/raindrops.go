d0c7836b3645cd91b61d9065d8f5102d54c258cc
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-10-07 18:07:20 +0100

    raindrops: map was broken, as order not guaranteed; use slice of strings instead; 1137ns/op

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -4,19 +4,21 @@ import (
 	"fmt"
 )
 
-// Easier to add new mappings
-var divisorMappings = map[int]string{
-	3: "Pling",
-	5: "Plang",
-	7: "Plong",
+var divisorMappings = []struct {
+	divisor int
+	output  string
+}{
+	{3, "Pling"},
+	{5, "Plang"},
+	{7, "Plong"},
 }
 
 // Convert a number to a string, the contents of which depend on the
 // number's factors.
 func Convert(input int) (output string) {
-	for div, str := range divisorMappings {
-		if input%div == 0 {
-			output += str
+	for _, mapping := range divisorMappings {
+		if input%mapping.divisor == 0 {
+			output += mapping.output
 		}
 	}
 
