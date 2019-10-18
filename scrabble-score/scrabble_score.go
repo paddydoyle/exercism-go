37774c2335bb8b805d8fb8d068f89cabfd2ad403
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-10-18 19:26:57 +0100

    scrabble-score: move the switch inside the loop; slight improvement to 468 ns/op

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -4,33 +4,27 @@ import (
 	"strings"
 )
 
-func scoreLookup(ch rune) (score int) {
-	switch ch {
-	case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
-		return 1
-	case 'D', 'G':
-		return 2
-	case 'B', 'C', 'M', 'P':
-		return 3
-	case 'F', 'H', 'V', 'W', 'Y':
-		return 4
-	case 'K':
-		return 5
-	case 'J', 'X':
-		return 8
-	case 'Q', 'Z':
-		return 10
-	default:
-		return 0
-	}
-}
-
 // Score returns the scrabble score for input string.
 func Score(input string) (score int) {
 	input = strings.ToUpper(input)
 
 	for _, ch := range input {
-		score += scoreLookup(ch)
+		switch ch {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score += 1
+		case 'D', 'G':
+			score += 2
+		case 'B', 'C', 'M', 'P':
+			score += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+		case 'K':
+			score += 5
+		case 'J', 'X':
+			score += 8
+		case 'Q', 'Z':
+			score += 10
+		}
 	}
 
 	return score
