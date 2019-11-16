42dba0526d077fa82639c6d4d8b7d51108dc8676
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-11-16 22:39:21 +0000

    isogram: working but slow, 5636ns/op

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,1 +1,23 @@
 package isogram
+
+import (
+	"strings"
+	"unicode"
+)
+
+// IsIsogram determines if the input word is an Isogram
+func IsIsogram(word string) bool {
+	charMap := make(map[rune]bool)
+
+	for _, ch := range strings.ToLower(word) {
+		if charMap[ch] {
+			return false
+		}
+
+		if unicode.IsLetter(ch) {
+			charMap[ch] = true
+		}
+	}
+
+	return true
+}
