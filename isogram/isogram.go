3ddd64386527cefd8fa653c742bb37738660e5ab
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-11-16 22:43:31 +0000

    isogram: pre-allocate map, slightly faster 4397ns/op

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -7,7 +7,7 @@ import (
 
 // IsIsogram determines if the input word is an Isogram
 func IsIsogram(word string) bool {
-	charMap := make(map[rune]bool)
+	charMap := make(map[rune]bool, len(word))
 
 	for _, ch := range strings.ToLower(word) {
 		if charMap[ch] {
