4b287bebf901ad04e31261cf1db7fb02e2b3d69a
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-11-16 23:15:26 +0000

    collatz: working

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,1 +1,22 @@
 package collatzconjecture
+
+import "errors"
+
+// CollatzConjecture returns the number of steps to reach 1
+func CollatzConjecture(number int) (steps int, err error) {
+	if number <= 0 {
+		return 0, errors.New("invalid input: must be strictly positive")
+	}
+
+	for number != 1 {
+		if number%2 == 0 {
+			number = number / 2
+		} else {
+			number = 3*number + 1
+		}
+
+		steps++
+	}
+
+	return steps, err
+}
