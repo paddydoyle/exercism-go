38a3a5130ebc4609ae3182499f9c8d45b26d01d4
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-11-16 22:10:41 +0000

    etl: works, but seems slow at 3910ns/op

--- etl/etl.go
+++ etl/etl.go
@@ -1,1 +1,18 @@
 package etl
+
+import (
+	"strings"
+)
+
+// Transform converts the int-to-chars map into a char-to-int map
+func Transform(input map[int][]string) map[string]int {
+	output := make(map[string]int)
+
+	for i, chars := range input {
+		for _, c := range chars {
+			output[strings.ToLower(c)] = i
+		}
+	}
+
+	return output
+}
