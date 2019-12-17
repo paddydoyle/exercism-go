eae6a9f2c1454b24298cae911d6e9a900a20a1b7
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-12-17 15:21:55 +0000

    diff-of-squares: working, but naive; 40, 63, 102 ns/op

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,1 +1,28 @@
 package diffsquares
+
+// SumOfSquares returns the sum of the first n natural number squares
+func SumOfSquares(n int) int {
+	sum := 0
+
+	for i := 1; i <= n; i++ {
+		sum += i * i
+	}
+
+	return sum
+}
+
+// SquareOfSum returns the square of the sum of the first n natural numbers
+func SquareOfSum(n int) int {
+	sum := 0
+
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+
+	return sum * sum
+}
+
+// Difference returns the difference between sum of squares, and square of sum
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
