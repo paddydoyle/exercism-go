9ff0fa6390aba5a2c30aa7af2deb79783c93611e
Paddy Doyle <paddy@tchpc.tcd.ie>
2020-01-14 22:53:31 +0000

    Revert "reverse_string: community solution, a lot slower though: 1845 ns/op vs 345 ns/op"
    This reverts commit ab518a30f3c96779f453e981157865aababd48b0.
    
    Go back to working solution as it's a lot faster.

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -2,13 +2,28 @@ package reverse
 
 // Reverse returns a reversal of the input string
 func Reverse(input string) string {
-	// solution copied from user kxnes
+	// solution copied from: https://yourbasic.org/golang/reverse-utf8-string/
 
-	reversed := make([]rune, 0)
+	// Same length
+	output := make([]byte, len(input))
 
-	for _, r := range input {
-		reversed = append([]rune{r}, reversed...)
+	// Keep track of previous input position (increasing),
+	// and output position (decreasing)
+	prevPos, outputPos := 0, len(input)
+
+	for pos := range input {
+		// For non-UTF8 strings, this will subtract 1
+		outputPos -= pos - prevPos
+
+		// Copy the rune/byte from the start of input to
+		// end of output
+		copy(output[outputPos:], input[prevPos:pos])
+
+		prevPos = pos
 	}
 
-	return string(reversed)
+	// Copy final rune to the start of the output
+	copy(output[0:], input[prevPos:])
+
+	return string(output)
 }
