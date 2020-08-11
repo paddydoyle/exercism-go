65e332f2ba1769d1efac4924f191c96d6887a403
Paddy Doyle <paddy.doyle@gmail.com>
2020-08-12 00:15:14 +0100

    sum_of_multiples: basic solution, very slow 931693 ns/op
    Use inclusion-exclusion principle to simplify

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -1,1 +1,24 @@
 package summultiples
+
+// SumMultiples calculates the sum of the multiples up to the limit
+func SumMultiples(limit int, divisors ...int) int {
+	set := make(map[int]bool)
+
+	for _, d := range divisors {
+		if d == 0 {
+			continue
+		}
+
+		for i := 1; i*d < limit; i++ {
+			set[i*d] = true
+		}
+	}
+
+	sum := 0
+
+	for key, _ := range set {
+		sum += key
+	}
+
+	return sum
+}
